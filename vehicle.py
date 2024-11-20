725a012e9f0714e8a79fa070355b84a8abf79d08
RuneSphinx <30443828+RuneSphinx@users.noreply.github.com>
2024-11-19 17:57:45 -0700

    Worked on vehicle class initial things

---
+++ vehicle.py
@@ -0,0 +1,12 @@
+# Parent Class
+class Vehicle:
+    
+    #class level attribute
+    wheels = 4
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+
